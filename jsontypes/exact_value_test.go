e1a94a659437b98ace71b5cd0854b15e95d1ce32
Benjamin Bennett <ben.bennett@hashicorp.com>
2024-04-24 16:01:28 +0200

    jsontypes: Replace usage of deprecated xattr.TypeWithValidate interface with xattr.ValidateableAttribute and xattr.ValidateableParameter (#50) (#54)

--- jsontypes/exact_value_test.go
+++ jsontypes/exact_value_test.go
@@ -4,12 +4,147 @@
 package jsontypes_test
 
 import (
+	"context"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
-	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
+	"github.com/hashicorp/terraform-plugin-framework/attr/xattr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
+	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/path"
+
+	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 )
+
+func TestExactValidateAttribute(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		exact         jsontypes.Exact
+		expectedDiags diag.Diagnostics
+	}{
+		"empty-struct": {
+			exact: jsontypes.Exact{},
+		},
+		"null": {
+			exact: jsontypes.NewExactNull(),
+		},
+		"unknown": {
+			exact: jsontypes.NewExactUnknown(),
+		},
+		"valid json object": {
+			exact: jsontypes.NewExactValue(`{"hello":"world", "array": [1, 2, 3]}`),
+		},
+		"valid json array": {
+			exact: jsontypes.NewExactValue(`["hello", "world"]`),
+		},
+		"invalid json - bracket mismatch": {
+			exact: jsontypes.NewExactValue(`{"hello":"world"`),
+			expectedDiags: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Invalid JSON String Value",
+					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
+						"Given Value: {\"hello\":\"world\"\n",
+				),
+			},
+		},
+		"invalid json - normal string": {
+			exact: jsontypes.NewExactValue("notvalidjson123"),
+			expectedDiags: diag.Diagnostics{
+				diag.NewAttributeErrorDiagnostic(
+					path.Root("test"),
+					"Invalid JSON String Value",
+					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
+						"Given Value: notvalidjson123\n",
+				),
+			},
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			resp := xattr.ValidateAttributeResponse{}
+
+			testCase.exact.ValidateAttribute(
+				context.Background(),
+				xattr.ValidateAttributeRequest{
+					Path: path.Root("test"),
+				},
+				&resp,
+			)
+
+			if diff := cmp.Diff(resp.Diagnostics, testCase.expectedDiags); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
+
+func TestExactValidateParameter(t *testing.T) {
+	t.Parallel()
+
+	testCases := map[string]struct {
+		exact           jsontypes.Exact
+		expectedFuncErr *function.FuncError
+	}{
+		"empty-struct": {
+			exact: jsontypes.Exact{},
+		},
+		"null": {
+			exact: jsontypes.NewExactNull(),
+		},
+		"unknown": {
+			exact: jsontypes.NewExactUnknown(),
+		},
+		"valid json object": {
+			exact: jsontypes.NewExactValue(`{"hello":"world", "array": [1, 2, 3]}`),
+		},
+		"valid json array": {
+			exact: jsontypes.NewExactValue(`["hello", "world"]`),
+		},
+		"invalid json - bracket mismatch": {
+			exact: jsontypes.NewExactValue(`{"hello":"world"`),
+			expectedFuncErr: function.NewArgumentFuncError(
+				0,
+				"Invalid JSON String Value: "+
+					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
+					"Given Value: {\"hello\":\"world\"\n",
+			),
+		},
+		"invalid json - normal string": {
+			exact: jsontypes.NewExactValue("notvalidjson123"),
+			expectedFuncErr: function.NewArgumentFuncError(
+				0,
+				"Invalid JSON String Value: "+
+					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
+					"Given Value: notvalidjson123\n",
+			),
+		},
+	}
+	for name, testCase := range testCases {
+		name, testCase := name, testCase
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+
+			resp := function.ValidateParameterResponse{}
+
+			testCase.exact.ValidateParameter(
+				context.Background(),
+				function.ValidateParameterRequest{
+					Position: 0,
+				},
+				&resp,
+			)
+
+			if diff := cmp.Diff(resp.Error, testCase.expectedFuncErr); diff != "" {
+				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
+			}
+		})
+	}
+}
 
 func TestExactUnmarshal(t *testing.T) {
 	t.Parallel()
--- jsontypes/normalized_type_test.go
+++ jsontypes/normalized_type_test.go
@@ -7,83 +7,11 @@ import (
 	"context"
 	"testing"
 
-	"github.com/google/go-cmp/cmp"
-	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
-	"github.com/hashicorp/terraform-plugin-framework/diag"
-	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-go/tftypes"
+
+	"github.com/hashicorp/terraform-plugin-framework-jsontypes/jsontypes"
 )
-
-func TestNormalizedTypeValidate(t *testing.T) {
-	t.Parallel()
-
-	testCases := map[string]struct {
-		in            tftypes.Value
-		expectedDiags diag.Diagnostics
-	}{
-		"empty-struct": {
-			in: tftypes.Value{},
-		},
-		"null": {
-			in: tftypes.NewValue(tftypes.String, nil),
-		},
-		"unknown": {
-			in: tftypes.NewValue(tftypes.String, tftypes.UnknownValue),
-		},
-		"valid json object": {
-			in: tftypes.NewValue(tftypes.String, `{"hello":"world", "array": [1, 2, 3]}`),
-		},
-		"valid json array": {
-			in: tftypes.NewValue(tftypes.String, `["hello", "world"]`),
-		},
-		"invalid json - bracket mismatch": {
-			in: tftypes.NewValue(tftypes.String, `{"hello":"world"`),
-			expectedDiags: diag.Diagnostics{
-				diag.NewAttributeErrorDiagnostic(
-					path.Root("test"),
-					"Invalid JSON String Value",
-					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
-						"Given Value: {\"hello\":\"world\"\n",
-				),
-			},
-		},
-		"invalid json - normal string": {
-			in: tftypes.NewValue(tftypes.String, "notvalidjson123"),
-			expectedDiags: diag.Diagnostics{
-				diag.NewAttributeErrorDiagnostic(
-					path.Root("test"),
-					"Invalid JSON String Value",
-					"A string value was provided that is not valid JSON string format (RFC 7159).\n\n"+
-						"Given Value: notvalidjson123\n",
-				),
-			},
-		},
-		"wrong-value-type": {
-			in: tftypes.NewValue(tftypes.Number, 123),
-			expectedDiags: diag.Diagnostics{
-				diag.NewAttributeErrorDiagnostic(
-					path.Root("test"),
-					"JSON Normalized Type Validation Error",
-					"An unexpected error was encountered trying to validate an attribute value. This is always an error in the provider. Please report the following to the provider developer:\n\n"+
-						"expected String value, received tftypes.Value with value: tftypes.Number<\"123\">",
-				),
-			},
-		},
-	}
-	for name, testCase := range testCases {
-		name, testCase := name, testCase
-		t.Run(name, func(t *testing.T) {
-			t.Parallel()
-
-			diags := jsontypes.NormalizedType{}.Validate(context.Background(), testCase.in, path.Root("test"))
-
-			if diff := cmp.Diff(diags, testCase.expectedDiags); diff != "" {
-				t.Errorf("Unexpected diagnostics (-got, +expected): %s", diff)
-			}
-		})
-	}
-}
 
 func TestNormalizedTypeValueFromTerraform(t *testing.T) {
 	t.Parallel()
